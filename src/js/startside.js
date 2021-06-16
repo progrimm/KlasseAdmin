1240a95551469df609705243062d55ccf73cefd0
Erik M. Haave <erik.haave@hotmail.no>
2021-06-16 20:57:53 +0200

    Ferdig med forside

--- src/js/startside.js
+++ src/js/startside.js
@@ -5,28 +5,6 @@ let data = JSON.parse(fs.readFileSync(dataFilename));
 
 window.onload = () => {
     klasseliste();
-    $("#btnVelgKlasse").onclick = () => {
-        let klasse = {
-            klassekode: $("#selKlasse").value,
-            elever: data[$("#selKlasse").value].elever
-        }
-        sessionStorage.setItem("valgtKlasse", JSON.stringify(klasse));
-        window.location = "fravaer.html";
-    }
-}
-
-// Midlertidig valg av klasse, fyller select-elementet
-function lagSelKlasse() {
-    $("#selKlasse").innerHTML = "";
-
-    let options = "<option disabled selected>Velg Klasse</option>";
-
-    for (klasseKode in data) {
-        options += "<option value'" + klasseKode + "'>" + klasseKode + "</option>";
-    }
-
-    $("#selKlasse").innerHTML = options;
-    data[klasseKode].elever.length
 }
 
 function klasseliste() {
