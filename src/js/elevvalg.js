1fc0a1f6f515a8f3ad4bc30ca456e7417defb1c3
jon2mter <jon.tomter@gmail.com>
2021-07-21 21:06:03 +0200

    misc

--- src/js/elevvalg.js
+++ src/js/elevvalg.js
@@ -9,67 +9,163 @@ const elever = valgtKlasse.elever;
 const alle_elever = data[klasse].elever;
 jobbeliste_elever = elever.slice(0);  // liste som det kan slettes elever fra under utvalg
 
+const ett_hakk_x = 170;
+const ett_hakk_y = 120;
+
 document.title = 'KlasseAdmin - ' + valgtKlasse.klassekode + " - Tilfeldig elevvalg";
 window.onload = () => {
     // Husker hvilken side man gikk til nedtelling fra
     sessionStorage.setItem("nedtelling_ref", window.location.pathname);
     
-    $('#klasse').innerHTML = klasse;
+    initialiser();
+    bunke = $('#bunkekort');
     antall_elever = $('#antall_elever');
     antall_elever.focus();  // fokuserer på inputfeltet
+    $('#klasse').innerHTML = klasse;
     // lyttere
+    antall_elever.oninput = bare2siffer;
     $('#btn_elever').onclick = velg_elever;
     onkeydown = function (evt) {    // hvis brukeren trykker på enter
         if (evt.keyCode === 13 && antall_elever === document.activeElement) velg_elever();
     }
     $('#bare_tilstedevaerende').onclick = 
     $('#uten_tilbakelegging').onclick = 
-    $('#nytt_valg').onclick = 
-        reinitialiser;
+        tilbakestill;
+    $('#nytt_valg').onclick = () => {
+        // $('#elev_utvalg').innerHTML='';
+        tilbakestill();
+    }
+    onresize = flytt_elever;
 }
 
-function reinitialiser() {
-    $('#elev_utvalg').innerHTML='';
-    $('#nytt_valg').hidden = true;
-    jobbeliste_elever = alle_elever.slice(0);
-        if ($('#bare_tilstedevaerende').checked) {      // hvis bare tilstedeværende elever skal velges
+function initialiser() {
+    if( ! $('#uten_tilbakelegging').checked) {      // valg med tilbakelegging
+        // oppdaterer liste for hvert valg
+        if ($('#bare_tilstedevaerende').checked) {  // hvis bare tilstedeværende elever skal velges
             jobbeliste_elever = elever.slice(0);
-        }
+        } else jobbeliste_elever = alle_elever.slice(0);
+    }
+    $('#elevantall').innerHTML = jobbeliste_elever.length;
+}
+
+function tilbakestill() {
+    $('#nytt_valg').hidden = true;
+    if ($('#bare_tilstedevaerende').checked) {  // hvis bare tilstedeværende elever skal velges
+        jobbeliste_elever = elever.slice(0);
+    } else jobbeliste_elever = alle_elever.slice(0);
+    $('#elevantall').innerHTML = jobbeliste_elever.length;
+    
     if($('#uten_tilbakelegging').checked) {
-        // ved valg uten tilbakelegging, skriver ut antall elever igjen
-        $('#elevantall').innerHTML='Antall elever igjen: '+jobbeliste_elever.length;
-    } else $('#elevantall').innerHTML='';
+        $('#elevantall').style.color = 'black';
+    } else $('#elevantall').style.color = 'gray';   // viser at antall er fast
+    // if ($('#elev_utvalg').innerHTML ==='<div><h3>Tomt for elever!</h3></div>') {
+    //     $('#elev_utvalg').innerHTML ='';
+    // }
 }
 
 function velg_elever() {
-    let em = $('#elev_utvalg');
-    em.innerHTML ='';
-    if (jobbeliste_elever.length === 0) {
-        em.innerHTML ='Tomt for elever!';
-        $('#nytt_valg').hidden = false;
-        return;
-    }
-    // hvis valgt antall overskrider antall elever, blir antallet maks antall elever
-    let antall = (+antall_elever.value < jobbeliste_elever.length ? +antall_elever.value : jobbeliste_elever.length);
-    for (var i=0; i<antall; i++) {
-        let tilfeldig_index = Math.floor((Math.random() * jobbeliste_elever.length));  // tilfeldig valg av 
-        let tilfeldig_valgt_elev = jobbeliste_elever[tilfeldig_index];                 // elev fra klasse
-        jobbeliste_elever.splice(tilfeldig_index,1);
-        // genererer nytt element
-        let nytt_em = document.createElement('div');
-        nytt_em.innerHTML = "<h3>"+tilfeldig_valgt_elev+"</h3>";   
-        em.appendChild(nytt_em);
-    }
+    // finner posisjon til bunke
+    let x_bunke = bunke.offsetLeft;
+    let y_bunke = bunke.offsetTop;
+
+    trekk_inn(x_bunke, y_bunke);
+    // let em = $('#elev_utvalg');
+    // em.innerHTML ='';
+    // if (jobbeliste_elever.length === 0) {
+    //     em.innerHTML ='<div><h3>Tomt for elever!</h3></div>';
+    //     return;
+    // }
+
+    setTimeout(() => {  // 1 sek
+        // hvis valgt antall overskrider antall elever, blir antallet maks antall elever
+        let antall = (+antall_elever.value < jobbeliste_elever.length ? +antall_elever.value : jobbeliste_elever.length);
+        for (var i=0; i<antall; i++) {
+            let tilfeldig_index = Math.floor((Math.random() * jobbeliste_elever.length));  // tilfeldig valg av 
+            let tilfeldig_valgt_elev = jobbeliste_elever[tilfeldig_index];                 // elev fra klasse
+            jobbeliste_elever.splice(tilfeldig_index,1);
+            
+            // generer elevkort
+            let elevkort = document.createElement('div');
+            elevkort.innerHTML = tilfeldig_valgt_elev;
+            elevkort.className = 'elevkort';
+            elevkort.style.left = (x_bunke-26)+'px';
+            elevkort.style.top = (y_bunke+26)+'px';
+            elevkort.style.transform = 'rotate(-90deg)';
+            $('#elev_utvalg').appendChild(elevkort);
+            // finner avstand som kortet skal flyttes
+            let flytt_x = -ett_hakk_x;  // første kolonne
+            let nr = i+1;
+            if((nr+1) % 3 === 0) flytt_x = 0;   // andre kolonne
+            else if (nr % 3 === 0) flytt_x = ett_hakk_x;    // tredje
     
-    // MÅ SMELTES SAMMEN MED INIT FUNC
-    // valg uten tilbakelegging
-    if ($('#uten_tilbakelegging').checked) {
-        $('#elevantall').innerHTML='Antall elever igjen: '+jobbeliste_elever.length;
-    } else {    // med tilbakelegging
-        // oppdaterer liste for hvert valg
-        jobbeliste_elever = alle_elever.slice(0);
-        if ($('#bare_tilstedevaerende').checked) {      // hvis bare tilstedeværende elever skal velges
-            jobbeliste_elever = elever.slice(0);
+            let rad_nr = Math.floor((nr-1) /3) + 1;
+            let flytt_y = ett_hakk_y * rad_nr + 30; // 30: klarering fra bunke
+            
+            setTimeout(() => {
+                elevkort.style.transform = 'translate('+flytt_x+'px, '+flytt_y+'px)';
+            }, 100);
         }
+        if (jobbeliste_elever.length === 0 && $('#uten_tilbakelegging').checked)
+            $('#nytt_valg').hidden = false;
+    
+        initialiser();  // reinitialiserer
+    }, 1010);
+}
+
+function trekk_inn(x, y) {
+    let elevkort = $('.elevkort');
+    console.log(elevkort);
+    if(!!elevkort[0]) console.log(elevkort[0].offsetTop);
+    if($('#uten_tilbakelegging').checked) {
+        for (const elev of elevkort) {
+            elev.style.left = '-330px';
+            elev.style.opacity = '0'; 
+        }
+    } else {
+        for (const elev of elevkort) {
+            elev.style.left = (x-26)+'px';
+            elev.style.top = (y+26)+'px';
+            elev.style.transform = 'rotate(90deg)'; 
+        }
+    }
+    setTimeout(() => {
+        $('#elev_utvalg').innerHTML ='';
+    }, 1000);
+
+
+    // let kort_paa_bordet = $('.kort_paa_bordet');
+    // sett alt på pause i 1 sek
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+    // for (const elev of elevkort) {
+    //     $('#kortbunke').removeChild(elev);        
+    // }
+    // while (elevkort.length < 0) {
+    //     const elev = elevkort[0];
+    //     $('#kortbunke').removeChild(elev);        
+        //     console.log(elev.offsetTop);
+        //     console.log(bunke.offsetTop);
+        //     console.log($('#kortbunke').offsetTop);
+        //     console.log('---');
+    //     // hvis kortene er på utsiden av presentasjonsområde
+    //     // if (elev.offsetLeft < 0 || elev.offsetTop < bunke.offsetTop+10) {   // liitt margin
+    //         // console.log('asd');
+    //     // } else index++;
+    // }
+}
+
+function flytt_elever() {
+    // finner posisjon til bunke
+    let x_bunke = bunke.offsetLeft;
+
+    let elevkort =$('.elevkort');
+    for (const elev of elevkort) {
+        elev.style.left = (x_bunke-26)+'px';
+    }
+}
+
+function bare2siffer() {  // lar det til en hver tid bare være to siffer i input
+    let antall = antall_elever.value;
+    if (antall.length > 2) {
+        antall_elever.value = antall.substr(antall.length - 2);
     }
 }
--- src/js/nedtelling.js
+++ src/js/nedtelling.js
@@ -17,21 +17,18 @@ window.onload = () => {
     veksle = $('#veksle_start_stopp');
     minutt = $('#minutt');
     sekund = $('#sekund');
-    minutt.value = nuller_foran(min);
-    sekund.value = nuller_foran(sek);
+    nuller_foran(min, sek)
+
     // lyttere
     $('#nullstill').onclick = nullstill;
     veksle.onclick = start_eller_stopp;
     // stopper klokka hvis bruker fokuserer på inputfeltene
-    minutt.onfocus = () => {
+    minutt.onfocus = sekund.onfocus = () => {
         if (paa_overtid) nullstill();   // nullstiller hvis klokka er på overtid selv når bruker trykker stopp
         else stopp_nedtelling();
-        minutt.select() // markerer tall
     }
-    sekund.onfocus = () => {
-        if (paa_overtid) nullstill();   // nullstiller hvis klokka er på overtid selv når bruker trykker stopp
-        else stopp_nedtelling();
-        sekund.select() // markerer tall
+    minutt.onkeydown = sekund.onkeydown = () => {
+        // 
     }
     // setter nye startverdier for min og sek hvis bruker gjør endringer
     minutt.onchange = sekund.onchange = () => {
@@ -41,7 +38,7 @@ window.onload = () => {
     minutt.oninput = sekund.oninput = () => {
         // behandler input slik at det bare er to siffer i input
         bare2siffer();
-        // formatering?
+        nuller_foran(minutt.value, sekund.value);
     }
 
     $('#lagre_tid').onclick = oppdater_standard_tid;
@@ -55,8 +52,7 @@ function nedteller() {
         min--;
     }
 
-    minutt.value = nuller_foran(min);
-    sekund.value = nuller_foran(sek);
+    nuller_foran(min, sek);
     if (min === 0 && sek === 0) { // nedtelling ferdig
         clearInterval(nedteller_intervall);
         // teller hvor mye nedtellingen er på overtid
@@ -67,7 +63,7 @@ function nedteller() {
         else new Audio('multimedia/gong.mp3').play();
 
         setTimeout(() => {
-            if (klokka_gaar) $('#minus').innerHTML = '-';
+            if (overtid_intervall) $('#minus').innerHTML = '-';
         }, 1000);    // minus foran tallene
         minutt.style.color = sekund.style.color = 'red'; // negative tall blir rød
     }
@@ -79,8 +75,7 @@ function start_eller_stopp() {
     if (sek >= 60) {    // ved for mange sekunder i input
         min += 1;       // blir aldri mer enn ett min ekstra
         sek %= 60;      // sekundene til overs
-        minutt.value = nuller_foran(min);
-        sekund.value = nuller_foran(sek);
+        nuller_foran(min, sek);
     }
 
     if (klokka_gaar) {
@@ -101,16 +96,16 @@ function stopp_nedtelling() {
     veksle.innerHTML = 'Start';
     veksle.className = 'btn btn-success';
     $('#minus').innerHTML = '';
-    $('#nedteller').style.color = minutt.style.color = sekund.style.color = 'black';
+    minutt.style.color = sekund.style.color = 'black';
     clearInterval(nedteller_intervall);
     clearInterval(overtid_intervall);
+    overtid_intervall = false;
 }
 
 function nullstill() {
     stopp_nedtelling();
     paa_overtid = false;
-    minutt.value = nuller_foran(start_min);
-    sekund.value = nuller_foran(start_sek);
+    nuller_foran(start_min, start_sek);
 }
 
 function overtid_stoppeklokke() {
@@ -120,26 +115,30 @@ function overtid_stoppeklokke() {
         sek = 0;
         min++;
     }
-    minutt.value = nuller_foran(min);
-    sekund.value = nuller_foran(sek);
+    nuller_foran(min, sek);
 }
 
-function nuller_foran(tall) {   // legger til nuller foran
+function nuller_foran(min_uten_0, sek_uten_0) {   // legger til nuller foran
     let plasser = 2;
-    let tall_med_0 = "00" + tall;
+    let min_med_0 = "00" + min_uten_0;
+    let sek_med_0 = "00" + sek_uten_0;
     // tar med så mange nuller som trengs
-    return tall_med_0.substr(tall_med_0.length - plasser);
+    minutt.value = min_med_0.substr(min_med_0.length - plasser);
+    sekund.value = sek_med_0.substr(sek_med_0.length - plasser);
 }
 
 function bare2siffer() {  // lar det til en hver tid bare være to siffer i input
     let min_str = minutt.value;
     if (min_str.length > 2) {
         minutt.value = min_str.substr(min_str.length - 2);
-    } else minutt.value = nuller_foran(min_str);
+        return;
+    }
     let sek_str = sekund.value;
     if (sek_str.length > 2) {
         sekund.value = sek_str.substr(sek_str.length - 2);
-    } else sekund.value = nuller_foran(sek_str);
+        return;
+    }
+    nuller_foran(min_str, sek_str);
 }
 
 function oppdater_standard_tid() {
