f2e46c5b00c834de48b64e40204fa78d3e5c9599
jon2mter <jon.tomter@gmail.com>
2021-08-12 00:51:09 +0200

    fravær tooltip med bugs

--- src/js/fravaer.js
+++ src/js/fravaer.js
@@ -28,21 +28,34 @@ function oppstart() {
     for (let i = 0; i < listeLengde; i++) {                                                  //Lager en div til hver elev
         let elevDiv = document.createElement("div");
         elevDiv.id = elever[i];
-        elevDiv.className = "elevDiv green";
+        elevDiv.className = "elevDiv green tooltip";
 
         let nyElevNavn = document.createElement("p");
         nyElevNavn.innerHTML = elever[i];
+        setTimeout(() => {
+            if (nyElevNavn.clientWidth < nyElevNavn.scrollWidth) {
+                let span = document.createElement('span');
+                span.onclick = () => { fravaer(elever[i]) };
+                span.className = 'tooltiptext';
+                span.innerHTML = elever[i];
+                nyElevNavn.appendChild(span);
+                setTimeout(() => {
+                    span.style.marginLeft = (span.offsetWidth /-2) +'px';
+                }, 50);
+            }
+        }, 50);
+
         nyElevNavn.style.pointerEvents = "none";
         nyElevNavn.style.userSelect = "none";
 
         if (elever_tilstede.includes(elever[i])) {   // hvis eleven er tilstedeværende
-            elevDiv.className = "elevDiv green";
+            elevDiv.classList.add('green');
         }
         else {  // hvis fraværende
-            elevDiv.className = "elevDiv red";
+            elevDiv.classList.add('red');
             eleverFravaer.push(elever[i]);
         }
-        elevDiv.onclick = fravaer;                                                     //Kjører funskjonen fravaer() når buttonen blir trykket
+        elevDiv.onclick = () => { fravaer(elever[i]) };                                                     //Kjører funskjonen fravaer() når buttonen blir trykket
 
         document.getElementById("elevListe").appendChild(elevDiv);                          //Legger til diven for eleven på nettsiden
         document.getElementById(elever[i]).appendChild(nyElevNavn);                         //Legger til navnet i den diven nettopp lagt til
@@ -60,19 +73,20 @@ function oppstart() {
     });
 }
 
-function fravaer(event) {
-    let elev = event.target.id;                                                      //Finner id-en til den eleven som får registrert fravær
-    let farge = (event.target.className).replace("elevDiv ", "");
-
-    if (farge === "green") {
-        document.getElementById(elev).className = "elevDiv red";
-
+function fravaer(elev) {
+    console.log(elev)
+    
+    if ($('#'+elev).classList.contains("green")) {
+        document.getElementById(elev).classList.remove('green');
+        document.getElementById(elev).classList.add('red');
+        
         let elevIndex = elever.indexOf(elev);                                               //Finner indexen til eleven registrert
         eleverFravaer.push(elever[elevIndex]);                                              //Legger til eleven i arrayet over de som får fravær
         elever.splice(elevIndex, 1);                                                        //Fjerner fra hovedlista
     }
     else {
-        document.getElementById(elev).className = "elevDiv green";
+        document.getElementById(elev).classList.remove('red');
+        document.getElementById(elev).classList.add('green');
 
         let elevIndex = eleverFravaer.indexOf(elev);
         elever.push(eleverFravaer[elevIndex]);
