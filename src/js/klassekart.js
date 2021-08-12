06a6a5252520acba24ed277e5bc393fe7d98b5cd
Åmund <aamgrim@gmail.com>
2021-08-12 02:34:51 +0200

    ting

--- src/js/klassekart.js
+++ src/js/klassekart.js
@@ -295,7 +295,9 @@ function visKlassekart(nyGenerering) {
                     elevID++
                 }
                 btnElev.innerHTML = elevNavn;
-                btnElev.title = "" + elevNavn;
+                if (elevNavn !== "&nbsp;") {
+                    btnElev.title = "" + elevNavn;
+                }
                 btnElev.addEventListener("click", byttePlass);  // Legger til hendelseslytter på elevene så man kan bytte plasser
                 $("#rad" + i + "kolonne" + j).appendChild(btnElev);
             }
